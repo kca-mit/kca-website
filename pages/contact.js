1f7e8431df511ecafc522222502ba403a00feea3
kca-mit <67454046+kca-mit@users.noreply.github.com>
2020-09-12 10:55:51 -0400

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -20,10 +20,11 @@ export default function (props) {
           <div className="contents typography">
             <h1>Contact</h1>
             <p>Hello!</p>
-            <p>For any inqueries you can email the exec team at: <a href="mailto:ksa-exec@mit.edu">ksa-exec@mit.edu</a></p>
-            <p>Follow KSA on Facebook for photos from the events and important announcements: <a href="https://www.facebook.com/MIT-KSA-198424940189002/">MIT KSA Facebook page.</a></p>
-            <p>To report a typo on the website or make a suggestion, email the current web-master: <a href="mailto:slv@mit.edu">slv@mit.edu</a></p>
-            <p>MIT KSA welcomes any donations. Please use the exec email listed above and contact us if you are interested to make a contribution to spreading Korean culture at MIT campus.</p>
+            <p>For any inqueries you can email the exec team at: <a href="mailto:kca-exec@mit.edu">kca-exec@mit.edu</a></p>
+            <p>For any job opportunties you can email <a href="mailto:kca-jobs@mit.edu">kca-jobs@mit.edu</a></p>
+            <p>Follow KCA on Facebook for photos from the events and important announcements: <a href="https://www.facebook.com/MIT-KSA-198424940189002/">MIT KCA Facebook page.</a></p>
+            <p>To report a typo on the website or make a suggestion, email the current web-master: <a href="mailto:jeyoon@mit.edu">jeyoon@mit.edu</a></p>
+            <p>MIT KCA welcomes any donations. Please use the exec email listed above and contact us if you are interested to make a contribution to spreading Korean culture at MIT campus.</p>
           </div>
           <a id="pusheen" href="https://www.youtube.com/watch?v=lJtHNEDnrnY"><img src={prefixLink(PusheenImg)} height={50}/></a>
         </div>
